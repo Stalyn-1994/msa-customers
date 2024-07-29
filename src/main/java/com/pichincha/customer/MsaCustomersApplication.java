699dd904adb062bf1d9d401e9cd6e7da9ffb6262
Stalyn-1994 <iguairac@pichincha.com>
2024-07-28 19:56:39 -0500

    (feature/customer): init repository why? To build with dependency neccesary how? Add dependencies framework and complement

---
+++ src/main/java/com/pichincha/customer/MsaCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCustomersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsaCustomersApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/pichincha/customer/MsaCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaCustomersApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
